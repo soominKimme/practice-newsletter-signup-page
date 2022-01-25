4af4af798004b2942872a4f6172683c3cadc2f43
Soomin Kim <96629550+soominKimme@users.noreply.github.com>
2022-01-25 20:09:47 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
