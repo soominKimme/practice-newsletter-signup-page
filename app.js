018f259987442f1024e4cf42ea8476f1763f0212
Soomin Kim <96629550+soominKimme@users.noreply.github.com>
2022-02-06 22:55:29 +0000

    Setup: Post method

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+})
+
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
