f33d6c32ce343aa76aa08ad50e45ce8231557bc9
Soomin Kim <96629550+soominKimme@users.noreply.github.com>
2022-02-06 22:50:47 +0000

    Setup: GET method

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.sendFile(__dirname + "/signup.html");
 });
-
-
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
