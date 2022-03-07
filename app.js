390feb735edf6ab132dc41fd16b81216ed0be285
Soomin Kim <96629550+soominKimme@users.noreply.github.com>
2022-03-07 17:28:51 +0000

    Feat: Post data to Mailchimp's servers via API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -13,13 +14,52 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
-})
+  const data = {
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/a2c2099482";
+
+  const options = {
+    method: "POST",
+    auth: "soo1:2a32fdc81fcd250440b6a927dfe9045a-us14"
+  }
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
 });
+
+
+// API Key
+// 2a32fdc81fcd250440b6a927dfe9045a-us14
+
+// List ID
+// a2c2099482
+
+//{"name":"Freddie'\''s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}
